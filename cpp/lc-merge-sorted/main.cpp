78dfbfd93bf2ee4764b08fc08ddb320b31af6d61
Zhansong Li <lizhansong@hvariant.com>
2019-02-11 16:55:01 +1100

    another solution using backward merging

--- cpp/lc-merge-sorted/main.cpp
+++ cpp/lc-merge-sorted/main.cpp
@@ -10,8 +10,34 @@
 class Solution {
 public:
   void merge(std::vector<int>& nums1, int m, const std::vector<int>& nums2, int n) {
-    std::copy_n(nums2.begin(), n, nums1.begin() + m);
-    std::inplace_merge(nums1.begin(), nums1.begin() + m, nums1.begin() + m + n);
+    // STL solution
+//    std::copy_n(nums2.begin(), n, nums1.begin() + m);
+//    std::inplace_merge(nums1.begin(), nums1.begin() + m, nums1.begin() + m + n);
+
+    // merge from back
+    auto idx = m + n - 1;
+    auto idx1 = m - 1;
+    auto idx2 = n - 1;
+    while(idx1 >= 0 && idx2 >= 0)
+    {
+      if(nums1[idx1] > nums2[idx2])
+      {
+        nums1[idx--] = nums1[idx1--];
+      }
+      else
+      {
+        nums1[idx--] = nums2[idx2--];
+      }
+    }
+
+    while(idx1 >= 0)
+    {
+      nums1[idx--] = nums1[idx1--];
+    }
+    while(idx2 >= 0)
+    {
+      nums1[idx--] = nums2[idx2--];
+    }
   }
 };
 
