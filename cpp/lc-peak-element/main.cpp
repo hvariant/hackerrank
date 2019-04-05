76460799fa4f301ce4927d1fbd372cc00b0c6ee2
Zhansong Li <zhansong.li@ir.com>
2019-04-05 15:21:50 +1100

    https://leetcode.com/problems/find-peak-element/

---
+++ cpp/lc-peak-element/main.cpp
@@ -0,0 +1,78 @@
+#define CATCH_CONFIG_FAST_COMPILE
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+#include <vector>
+
+class Solution {
+public:
+   int findPeakElement(const std::vector<int>& nums)
+   {
+      assert(nums.size() > 0);
+      if (nums.size() == 1)
+      {
+         return 0;
+      }
+
+      int left = 0, right = nums.size();
+      const auto is_peak = [&nums](int i)
+      {
+         if (i == 0)
+         {
+            return nums[i] > nums[i + 1];
+         }
+         if (i == nums.size() - 1)
+         {
+            return nums[i - 1] < nums[i];
+         }
+         return nums[i - 1] < nums[i] && nums[i] > nums[i + 1];
+      };
+
+      // invariant: nums[left] > nums[left-1] && nums[right] > nums[right+1]
+      while (left < right)
+      {
+         size_t mid = (right - left) / 2 + left;
+         if (is_peak(mid))
+         {
+            return mid;
+         }
+
+         // edge case with left + 1 == right
+         if (mid == left)
+         {
+            left = left + 1;
+         }
+         else // mid > left
+         {
+            // there must be a peak on the left side
+            if (nums[mid] < nums[mid - 1])
+            {
+               right = mid - 1;
+            }
+            // otherwise, the peak is on the right side
+            else
+            {
+               while (mid < right && nums[mid] >= nums[mid + 1]) mid++;
+               left = mid;
+            }
+         }
+      }
+
+      return left;
+   }
+};
+
+TEST_CASE("Example1")
+{
+  Solution solution;
+  CHECK(solution.findPeakElement({ 1,2,3,1 }) == 2);
+}
+
+TEST_CASE("Example2")
+{
+  Solution solution;
+  auto v = solution.findPeakElement({ 1,2,1,3,5,6,4 });
+  bool correct = (v == 1 || v == 5);
+  CHECK(correct);
+}
+
