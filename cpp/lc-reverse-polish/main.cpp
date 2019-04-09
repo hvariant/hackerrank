982b345c09492eb77b1e86d7523abc1519fc27db
Zhansong Li <lizhansong@hvariant.com>
2019-04-09 23:31:59 +1000

    https://leetcode.com/problems/evaluate-reverse-polish-notation/

---
+++ cpp/lc-reverse-polish/main.cpp
@@ -0,0 +1,56 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/578/
+
+#define CATCH_CONFIG_FAST_COMPILE
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+#include <vector>
+#include <stack>
+
+class Solution {
+public:
+    int evalRPN(const std::vector<std::string>& tokens)
+    {
+        std::stack<int> S;
+
+        for(const auto& token : tokens)
+        {
+            if(token == "*" || token == "/" || token == "+" || token == "-")
+            {
+                auto op2 = S.top();
+                S.pop();
+                auto& op1 = S.top();
+
+                switch(token[0])
+                {
+                case '*':
+                    op1 = op1 * op2;
+                    break;
+                case '/':
+                    op1 = op1 / op2;
+                    break;
+                case '+':
+                    op1 = op1 + op2;
+                    break;
+                case '-':
+                    op1 = op1 - op2;
+                    break;
+                }
+            }
+            else
+            {
+                int n = std::stoi(token);
+                S.push(n);
+            }
+        }
+
+        return S.top();
+    }
+};
+
+TEST_CASE("example")
+{
+    CHECK(Solution().evalRPN({"2", "1", "+", "3", "*"}) == 9);
+    CHECK(Solution().evalRPN({"4", "13", "5", "/", "+"}) == 6);
+    CHECK(Solution().evalRPN({"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"}) == 22);
+}
