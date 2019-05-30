26c4470bd7b3291bdc90fcbca5584dabf38a23c5
Zhansong Li <zhansong.li@ir.com>
2019-05-30 16:01:54 +1000

    actually O(n) but slower :/

--- cpp/lc-longest-consecutive-sequence/main.cpp
+++ cpp/lc-longest-consecutive-sequence/main.cpp
@@ -3,29 +3,67 @@
 #include <catch2/catch.hpp>
 
 #include <vector>
+#include <unordered_set>
 
 class Solution {
 public:
-    size_t longestConsecutive(std::vector<int>& nums)
-    {
-       if (nums.empty()) return 0;
+   //size_t longestConsecutive(std::vector<int>& nums)
+   //{
+   //   if (nums.empty()) return 0;
 
-       std::sort(nums.begin(), nums.end());
-       // remove duplicates:
-       //   https://stackoverflow.com/questions/1041620/whats-the-most-efficient-way-to-erase-duplicates-and-sort-a-vector
-       nums.erase( std::unique( nums.begin(), nums.end() ), nums.end() );
+   //   std::sort(nums.begin(), nums.end());
+   //   // remove duplicates:
+   //   //   https://stackoverflow.com/questions/1041620/whats-the-most-efficient-way-to-erase-duplicates-and-sort-a-vector
+   //   nums.erase( std::unique( nums.begin(), nums.end() ), nums.end() );
 
-       size_t r = 1;
-       size_t i = 0;
-       while (i < nums.size())
-       {
-          size_t j = i + 1;
-          while (j < nums.size() && nums[j] == nums[j - 1] + 1) j++;
-          r = std::max(r, j - i);
-          i = j;
-       }
-       return r;
-    }
+   //   size_t r = 1;
+   //   size_t i = 0;
+   //   while (i < nums.size())
+   //   {
+   //      size_t j = i + 1;
+   //      while (j < nums.size() && nums[j] == nums[j - 1] + 1) j++;
+   //      r = std::max(r, j - i);
+   //      i = j;
+   //   }
+   //   return r;
+   //}
+
+   int longestConsecutive(const std::vector<int>& nums)
+   {
+      if (nums.empty()) return 0;
+
+      std::unordered_set<int> S(nums.begin(), nums.end());
+      int r = 1;
+      for (auto n : nums)
+      {
+         if (S.count(n) == 0)
+         {
+            continue;
+         }
+
+         S.erase(n);
+
+         int lower = n - 1;
+         while (S.count(lower) > 0)
+         {
+            S.erase(lower);
+            lower--;
+         }
+         lower++;
+
+         int upper = n + 1;
+         while (S.count(upper) > 0)
+         {
+            S.erase(upper);
+            upper++;
+         }
+         upper--;
+
+         r = std::max(r, upper - lower + 1);
+      }
+
+      return r;
+   }
 };
 
 TEST_CASE("examples")
